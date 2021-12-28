6279bdd93ac54602fc3e2b3cc5ea7106bf547ff4
Chollamuthu Narayanan <chollamuthu.narayanan@autodesk.com>
2021-12-28 15:55:02 +0800

    Initial

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Configure the Microsoft Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
